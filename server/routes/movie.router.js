970bc7938ae83b9f5ea3a4f6e34214d9f1b731b4
KaetiG <kaetigust@gmail.com>
2019-06-21 16:43:49 -0500

    create routers folder and router file-import and export to server js

---
+++ server/routes/movie.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+//GET MOVIES
+//GET GENRES
+//POST EDITS
+
+module.exports = router;
