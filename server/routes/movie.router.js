740dd7a228453e38f42a329c7ab8220df6128163
KaetiG <kaetigust@gmail.com>
2019-06-21 16:59:13 -0500

    add GET route to retrieve movies from database-tested in Postman-is working

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,8 +2,19 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-//GET MOVIES
-//GET GENRES
-//POST EDITS
+//GET MOVIES REQUEST
+router.get('/', (req, res) => {
+    const databaseQuery = 'SELECT * FROM movies';
+    pool.query(databaseQuery)
+      .then((result) => { res.send(result.rows); })
+      .catch((err) => {
+        console.log('Error completing SELECT movie query', err);
+        res.sendStatus(500);
+      });
+  });
+//GET GENRES REQUEST
+
+//PUT REQUEST
+
 
 module.exports = router;
