f9aad57060c5ac348255959ab944a02b8ce782aa
KaetiG <kaetigust@gmail.com>
2019-06-22 12:59:34 -0500

    create details component

---
+++ src/components/Details/Details.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Details extends Component {
+    render() {
+        return(
+            <></>
+        )
+    }
+}
+
+export default connect(mapReduxStateToProps)(MovieList);
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
+//import axios from 'axios';
 import SingleMovie from '../SingleMovie/SingleMovie';
 
 class MovieList extends Component {
     componentDidMount() {
         this.props.dispatch({ type: "GET_MOVIES" });
+        //this.props.dispatch({ type: "GET_GENRES" });
     }
     render() {
         return (
