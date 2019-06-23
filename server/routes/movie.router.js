8b94c7004b64a1e4509d52c85be02a20062aade9
KaetiG <kaetigust@gmail.com>
2019-06-23 13:48:04 -0500

    create PUT query on server side router-tested in Postman-is working

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -34,6 +34,34 @@ router.get('/details/:id', (req, res) => {
     })
 })
 //PUT REQUEST
+router.put('/edit/:id', (req, res)=>{
+  pool.query(`UPDATE "movies"
+  SET "description" = 'Ferngully but on another planet and with special guests: the Smirfs'
+  WHERE "movies"."id" = $1;`, [req.params.id])
+  .then((result) => { res.send(result.rows); })
+  .catch((err)=> {
+    console.log('error updating movie', err);
+    res.sendStatus(500);
+  })
+})
 
+// router.put('/', (req, res) => {
+//   const updatedMovie = req.body;
+
+//   const queryText = `UPDATE "movies"
+//   SET "description" = 'Ferngully but on another planet and with special guests: the Blue man group'
+//   WHERE "movies"."id" = $1;`;
+
+//   const queryValues = [
+//     updatedMovie.description
+//   ];
+
+//   pool.query(queryText, queryValues)
+//     .then(() => { res.sendStatus(200); })
+//     .catch((err) => {
+//       console.log('Error completing PUT query', err);
+//       res.sendStatus(500);
+//     });
+// });
 
 module.exports = router;
--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Edit extends Component {
-    render(){
-        return(
+    state = {
+        description: '',
+    }
+    handleDescriptionChange = (event) => {
+        console.log('event happened')
+        this.setState({
+            description: event.target.value,
+        }
+        );
+    }
+    render() {
+        return (
             <>
-            <input placeholder='Example: Horror'></input>
-            <button>Add Genre</button>
+                <textarea type="text"
+                    rows="8"
+                    cols="55"
+                    onChange={this.handleDescriptionChange}
+                    value={this.state.description}
+                    key={'description'}>
+                </textarea>
+                <br />
+                {/* <input placeholder='New Description Here'
+            type='text' value={this.state.newDesu.description} 
+            onChange={this.handleDescriptionChange('description')}>
+            </input> */}
+                <button onClick={this.editMovie}>Submit</button>
             </>
         )
     }
